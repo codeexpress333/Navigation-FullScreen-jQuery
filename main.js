6d8eb8b783ec7c7255249e316b6d4568afcc82e4
differentdeepak <dk20071993@gmail.com>
2024-03-08 19:18:32 +0530

    navigation

---
+++ main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    var trigger = $('.navTrigger');
+
+    trigger.on('click', function () {
+        $(this).toggleClass('active');
+        $(this).next().toggleClass('active');
+    });
+    
+});
+
